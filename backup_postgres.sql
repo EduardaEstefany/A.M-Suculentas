0d480c918f8af548f357ab4e7fb4e01aa1f55915
JoãoMarRib <joaomarcosribeiro2001@gmail.com>
2025-09-23 19:59:59 -0300

    Banco de dados atualizado

--- backup_postgres.sql
+++ backup_postgres.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict B8iw1aKze28gjyeHlW9BZDghoSa8FAtmBggeBoi7nhu3uOghgSTy44COwwogt1M
+\restrict AcveFure1RNrgfofm1vxKwSCCSqnbYdFyGSl2oV7gZtQ42qbq3X7dDk4pWQnJ2G
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
 
--- Started on 2025-09-20 16:49:46
+-- Started on 2025-09-23 19:51:50
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -34,8 +34,7 @@ CREATE TABLE public.clientes (
     id integer NOT NULL,
     nome character varying(100) NOT NULL,
     email character varying(100) NOT NULL,
-    telefone character varying(20),
-    endereco text
+    telefone character varying(20)
 );
 
 
@@ -58,7 +57,7 @@ CREATE SEQUENCE public.clientes_id_seq
 ALTER SEQUENCE public.clientes_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4949 (class 0 OID 0)
+-- TOC entry 4924 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: clientes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -67,105 +66,24 @@ ALTER SEQUENCE public.clientes_id_seq OWNED BY public.clientes.id;
 
 
 --
--- TOC entry 224 (class 1259 OID 16507)
--- Name: itens_pedido; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.itens_pedido (
-    id integer NOT NULL,
-    pedido_id integer,
-    produto_id integer,
-    quantidade integer NOT NULL,
-    preco_unitario numeric(10,2) NOT NULL
-);
-
-
-ALTER TABLE public.itens_pedido OWNER TO postgres;
-
---
--- TOC entry 223 (class 1259 OID 16506)
--- Name: itens_pedido_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.itens_pedido_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.itens_pedido_id_seq OWNER TO postgres;
-
---
--- TOC entry 4950 (class 0 OID 0)
--- Dependencies: 223
--- Name: itens_pedido_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.itens_pedido_id_seq OWNED BY public.itens_pedido.id;
-
-
---
--- TOC entry 222 (class 1259 OID 16493)
--- Name: pedidos; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.pedidos (
-    id integer NOT NULL,
-    cliente_id integer,
-    data_pedido timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    status character varying(20) DEFAULT 'pendente'::character varying
-);
-
-
-ALTER TABLE public.pedidos OWNER TO postgres;
-
---
--- TOC entry 221 (class 1259 OID 16492)
--- Name: pedidos_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.pedidos_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.pedidos_id_seq OWNER TO postgres;
-
---
--- TOC entry 4951 (class 0 OID 0)
--- Dependencies: 221
--- Name: pedidos_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.pedidos_id_seq OWNED BY public.pedidos.id;
-
-
---
--- TOC entry 226 (class 1259 OID 16525)
+-- TOC entry 222 (class 1259 OID 16525)
 -- Name: posts; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.posts (
     id integer NOT NULL,
-    produto_id integer,
     titulo character varying(100),
     descricao text,
     data_criacao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    ativo boolean DEFAULT true
+    ativo boolean DEFAULT true,
+    cliente_id integer
 );
 
 
 ALTER TABLE public.posts OWNER TO postgres;
 
 --
--- TOC entry 225 (class 1259 OID 16524)
+-- TOC entry 221 (class 1259 OID 16524)
 -- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -181,8 +99,8 @@ CREATE SEQUENCE public.posts_id_seq
 ALTER SEQUENCE public.posts_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4952 (class 0 OID 0)
--- Dependencies: 225
+-- TOC entry 4925 (class 0 OID 0)
+-- Dependencies: 221
 -- Name: posts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -225,7 +143,7 @@ CREATE SEQUENCE public.produtos_id_seq
 ALTER SEQUENCE public.produtos_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4953 (class 0 OID 0)
+-- TOC entry 4926 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: produtos_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -234,7 +152,7 @@ ALTER SEQUENCE public.produtos_id_seq OWNED BY public.produtos.id;
 
 
 --
--- TOC entry 4762 (class 2604 OID 16463)
+-- TOC entry 4752 (class 2604 OID 16463)
 -- Name: clientes id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -242,23 +160,7 @@ ALTER TABLE ONLY public.clientes ALTER COLUMN id SET DEFAULT nextval('public.cli
 
 
 --
--- TOC entry 4769 (class 2604 OID 16510)
--- Name: itens_pedido id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.itens_pedido ALTER COLUMN id SET DEFAULT nextval('public.itens_pedido_id_seq'::regclass);
-
-
---
--- TOC entry 4766 (class 2604 OID 16496)
--- Name: pedidos id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.pedidos ALTER COLUMN id SET DEFAULT nextval('public.pedidos_id_seq'::regclass);
-
-
---
--- TOC entry 4770 (class 2604 OID 16528)
+-- TOC entry 4756 (class 2604 OID 16528)
 -- Name: posts id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -266,7 +168,7 @@ ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_
 
 
 --
--- TOC entry 4763 (class 2604 OID 16481)
+-- TOC entry 4753 (class 2604 OID 16481)
 -- Name: produtos id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -274,62 +176,42 @@ ALTER TABLE ONLY public.produtos ALTER COLUMN id SET DEFAULT nextval('public.pro
 
 
 --
--- TOC entry 4935 (class 0 OID 16460)
+-- TOC entry 4914 (class 0 OID 16460)
 -- Dependencies: 218
 -- Data for Name: clientes; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.clientes (id, nome, email, telefone, endereco) FROM stdin;
+COPY public.clientes (id, nome, email, telefone) FROM stdin;
 \.
 
 
 --
--- TOC entry 4941 (class 0 OID 16507)
--- Dependencies: 224
--- Data for Name: itens_pedido; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.itens_pedido (id, pedido_id, produto_id, quantidade, preco_unitario) FROM stdin;
-\.
-
-
---
--- TOC entry 4939 (class 0 OID 16493)
+-- TOC entry 4918 (class 0 OID 16525)
 -- Dependencies: 222
--- Data for Name: pedidos; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.pedidos (id, cliente_id, data_pedido, status) FROM stdin;
-\.
-
-
---
--- TOC entry 4943 (class 0 OID 16525)
--- Dependencies: 226
 -- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.posts (id, produto_id, titulo, descricao, data_criacao, ativo) FROM stdin;
+COPY public.posts (id, titulo, descricao, data_criacao, ativo, cliente_id) FROM stdin;
 \.
 
 
 --
--- TOC entry 4937 (class 0 OID 16478)
+-- TOC entry 4916 (class 0 OID 16478)
 -- Dependencies: 220
 -- Data for Name: produtos; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.produtos (id, nome, descricao, preco, estoque, promocao, ativo, imagem) FROM stdin;
-2	Suculenta 2	\N	20.00	10	20	t	/assets/produtosImg/produto-2.jpg
-5	Suculenta 5	\N	5.00	25	5	t	/assets/produtosImg/produto-5.png
-4	Suculenta 4	\N	10.00	20	10	t	/assets/produtosImg/produto-4.png
-3	Suculenta 3	\N	15.00	15	15	t	/assets/produtosImg/produto-3.jpg
 1	Suculenta 1	\N	25.00	5	0	t	/assets/produtosImg/produto-1.jpg
+2	Suculenta 2	\N	20.00	10	20	t	/assets/produtosImg/produto-2.jpg
+3	Suculenta 3	\N	15.00	15	15	t	/assets/produtosImg/produto-3.jpg
+4	Suculenta 4	\N	10.00	20	10	t	/assets/produtosImg/produto-4.png
+5	Suculenta 5	\N	5.00	25	5	t	/assets/produtosImg/produto-5.png
 \.
 
 
 --
--- TOC entry 4954 (class 0 OID 0)
+-- TOC entry 4927 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: clientes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -338,26 +220,8 @@ SELECT pg_catalog.setval('public.clientes_id_seq', 1, false);
 
 
 --
--- TOC entry 4955 (class 0 OID 0)
--- Dependencies: 223
--- Name: itens_pedido_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.itens_pedido_id_seq', 1, false);
-
-
---
--- TOC entry 4956 (class 0 OID 0)
+-- TOC entry 4928 (class 0 OID 0)
 -- Dependencies: 221
--- Name: pedidos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.pedidos_id_seq', 1, false);
-
-
---
--- TOC entry 4957 (class 0 OID 0)
--- Dependencies: 225
 -- Name: posts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -365,7 +229,7 @@ SELECT pg_catalog.setval('public.posts_id_seq', 1, false);
 
 
 --
--- TOC entry 4958 (class 0 OID 0)
+-- TOC entry 4929 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: produtos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -374,7 +238,7 @@ SELECT pg_catalog.setval('public.produtos_id_seq', 5, true);
 
 
 --
--- TOC entry 4774 (class 2606 OID 16469)
+-- TOC entry 4760 (class 2606 OID 16469)
 -- Name: clientes clientes_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -383,7 +247,7 @@ ALTER TABLE ONLY public.clientes
 
 
 --
--- TOC entry 4776 (class 2606 OID 16467)
+-- TOC entry 4762 (class 2606 OID 16467)
 -- Name: clientes clientes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -392,25 +256,7 @@ ALTER TABLE ONLY public.clientes
 
 
 --
--- TOC entry 4782 (class 2606 OID 16512)
--- Name: itens_pedido itens_pedido_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.itens_pedido
-    ADD CONSTRAINT itens_pedido_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 4780 (class 2606 OID 16500)
--- Name: pedidos pedidos_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.pedidos
-    ADD CONSTRAINT pedidos_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 4784 (class 2606 OID 16534)
+-- TOC entry 4766 (class 2606 OID 16534)
 -- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -419,7 +265,7 @@ ALTER TABLE ONLY public.posts
 
 
 --
--- TOC entry 4778 (class 2606 OID 16486)
+-- TOC entry 4764 (class 2606 OID 16486)
 -- Name: produtos produtos_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -428,46 +274,19 @@ ALTER TABLE ONLY public.produtos
 
 
 --
--- TOC entry 4786 (class 2606 OID 16513)
--- Name: itens_pedido itens_pedido_pedido_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.itens_pedido
-    ADD CONSTRAINT itens_pedido_pedido_id_fkey FOREIGN KEY (pedido_id) REFERENCES public.pedidos(id);
-
-
---
--- TOC entry 4787 (class 2606 OID 16518)
--- Name: itens_pedido itens_pedido_produto_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.itens_pedido
-    ADD CONSTRAINT itens_pedido_produto_id_fkey FOREIGN KEY (produto_id) REFERENCES public.produtos(id);
-
-
---
--- TOC entry 4785 (class 2606 OID 16501)
--- Name: pedidos pedidos_cliente_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.pedidos
-    ADD CONSTRAINT pedidos_cliente_id_fkey FOREIGN KEY (cliente_id) REFERENCES public.clientes(id);
-
-
---
--- TOC entry 4788 (class 2606 OID 16535)
--- Name: posts posts_produto_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 4767 (class 2606 OID 24757)
+-- Name: posts fk_cliente; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.posts
-    ADD CONSTRAINT posts_produto_id_fkey FOREIGN KEY (produto_id) REFERENCES public.produtos(id);
+    ADD CONSTRAINT fk_cliente FOREIGN KEY (cliente_id) REFERENCES public.clientes(id);
 
 
--- Completed on 2025-09-20 16:49:47
+-- Completed on 2025-09-23 19:51:50
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict B8iw1aKze28gjyeHlW9BZDghoSa8FAtmBggeBoi7nhu3uOghgSTy44COwwogt1M
+\unrestrict AcveFure1RNrgfofm1vxKwSCCSqnbYdFyGSl2oV7gZtQ42qbq3X7dDk4pWQnJ2G
 
